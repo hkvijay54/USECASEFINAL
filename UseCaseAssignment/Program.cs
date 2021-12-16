31df9c1520215e21a9d260202825afa644d9eb9e
hkvijay54 <hkvijay54@gmail.com>
2021-12-16 18:31:38 +0530

    uc-6 program

--- UseCaseAssignment/Program.cs
+++ UseCaseAssignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-// check employee Wage on part time or full time monthly.
+// Calculate Wages till a condition of total working hours or
+// days is reached for a month
 class Program
 {
     static void Main(string[] args)
@@ -9,14 +10,16 @@ class Program
         int EMP_RATE_PER_HOUR = 20;
         const int IS_PART_TIME = 2;
         const int NUM_OF_WORKING_DAYS = 20;
+        const int MAX_HRS_IN_MONTH = 100;
 
         // Variables
         int emphrs = 0;
-        int empwage = 0;
-        int totalSalary = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS )
         {
+            totalWorkingDays++;
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
@@ -33,9 +36,10 @@ class Program
                     emphrs = 0;
                     break;
             }
-            empwage = emphrs * EMP_RATE_PER_HOUR;
-            totalSalary = totalSalary + empwage;
+            totalEmpHrs = totalEmpHrs + emphrs;
+            Console.WriteLine("Working day " + totalWorkingDays + " Employee hours " + emphrs);
         }
-        Console.WriteLine(" monthly salry is " + totalSalary);
+        int totalSalary = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine(" Total salary is " + totalSalary);
     }
 }
