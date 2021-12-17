18ddfbb6e5c1aa98ce10626a7d6563c9e3e4f7b8
hkvijay54 <hkvijay54@gmail.com>
2021-12-17 17:15:58 +0530

    Line Program

--- UseCaseAssignment/Program.cs
+++ UseCaseAssignment/Program.cs
@@ -5,41 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        //constants
-        const int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
-        const int IS_PART_TIME = 2;
-        const int NUM_OF_WORKING_DAYS = 20;
-        const int MAX_HRS_IN_MONTH = 100;
-
-        // Variables
-        int emphrs = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-
-        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS )
-        {
-            totalWorkingDays++;
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
-            {
-                case IS_FULL_TIME:
-                    emphrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    emphrs = 4;
-                    break;
-                default:
-                    emphrs = 0;
-                    break;
-            }
-            totalEmpHrs = totalEmpHrs + emphrs;
-            Console.WriteLine("Working day " + totalWorkingDays + " Employee hours " + emphrs);
-        }
-        int totalSalary = totalEmpHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine(" Total salary is " + totalSalary);
+        Console.WriteLine("Enter two pairs of number (x1,y1)(x2,y2)");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        int x2 = Convert.ToInt32(Console.ReadLine());
+        int y2 = Convert.ToInt32(Console.ReadLine());
+        double diffx = Math.Pow((x2 - x1),2);
+        double diffy=Math.Pow((y2 - y1),2);
+        double res = Math.Sqrt(diffx + diffy);
+        Console.WriteLine(res);
     }
 }
